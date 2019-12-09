75569328a367db1601c8182659168ea05e9d3021
dianynf <dianbugas@gmail.com>
2019-12-09 22:10:44 +0700

    CRUD

---
+++ storage/framework/views/bf6ff2c9b5d530e465bca960afaab546834d66c5.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('title', 'About'); ?>
+    
+<?php $__env->startSection('container'); ?>
+    
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-2">Selamat Datang Bos</h1>
+        </div>
+        </div>
+</div>    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/coballaravel/resources/views/about.blade.php ENDPATH**/ ?>
