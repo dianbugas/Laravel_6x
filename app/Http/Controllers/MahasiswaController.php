0da70f3b98834ac1a204555b432cbb2c32c39c91
dianynf <dianbugas@gmail.com>
2019-12-18 23:01:25 +0700

    s

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
@@ -13,6 +14,8 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
+        $mahasiswa = DB::table('mahasiswa')->get();
+        dump($mahasiswa);
         return view('mahasiswa.index'); 
     }
 
